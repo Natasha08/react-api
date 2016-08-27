5ebaf1622a7a1a92df50e81921ec8752f0a37400
natasha <natasha08@me.com>
2016-08-27 13:44:10 -0600

    finish 'GET' todos page spec

--- api/spec/features/get_todos_spec.rb
+++ api/spec/features/get_todos_spec.rb
@@ -4,10 +4,8 @@ feature " 'GET TODOS' route " do
   let!(:todo) { create :todo }
   scenario "Todos Page renders json" do
     visit todos_path
+    
     expect(page).to have_content :json
-  end
-  scenario "json matches todo data" do
-    visit todos_path
-
+    expect(page).to have_text 'Shopping'
   end
 end
