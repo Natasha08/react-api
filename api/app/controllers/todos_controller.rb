90348476f20a2a4a178b78b3c7b0d24ba559f73e
natasha <natasha08@me.com>
2016-08-27 09:41:24 -0600

    add CORS config action index to respond with json

--- api/app/controllers/todos_controller.rb
+++ api/app/controllers/todos_controller.rb
@@ -1,6 +1,9 @@
 class TodosController < ApplicationController
+  before_action :set_headers
+
   def index
     @todos = Todo.all
+    render json: @todos
   end
 
   def new
@@ -19,6 +22,13 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find(params[:id])
+  end
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
   private
